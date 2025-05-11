e364885db278bab60925e31bb9937e28ab84fc49
enbesu <hau99082005@gmail.com>
2025-05-11 17:12:51 +0700

    thay đổi login và css

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,106 +8,86 @@ import {
   MDBCard,
   MDBCardBody,
   MDBInput,
+  MDBCheckbox,
 }
 from 'mdb-react-ui-kit';
+import { FaFacebook, FaGoogle, FaInstagram } from 'react-icons/fa';
 import Link from 'next/link';
-import { FaGoogle } from 'react-icons/fa';
-import { FaInstagramSquare } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
 
 function App() {
   return (
-  <MDBContainer
-  fluid
-  className="w-full"
-  style={{
-    backgroundImage: `url('/image/monday_1.webp')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backdropFilter: 'blur(5px)',
-  }}
->
-  <MDBRow className="d-flex justify-content-center align-items-center h-100">
-    <MDBCol col="12">
-      <MDBCard
-        className="my-5 mx-auto shadow-lg"
-        style={{
-          borderRadius: '1rem',
-          maxWidth: '400px',
-          background: 'rgba(255, 255, 255, 0.25)',
-          border: '1px solid #ccc',
-        }}
-      >
-        <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100 text-center">
-          <h2 className="fw-bold mb-2 text-dark">Đăng Nhập</h2>
-          <p className="text-[#1d1d1d] mb-4" style={{ fontSize: '15px', fontFamily: 'Lato' }}>
-            Vui lòng đăng nhập để nhận ưu đãi hấp dẫn!
+    <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
+
+      <MDBRow>
+
+        <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
+
+          <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{color: 'hsl(218, 81%, 95%)', zIndex: 999}}>
+            Chào mừng bạn đến với <br />
+            <span style={{color: 'hsl(218, 81%, 75%)'}}>hệ thống đặt vé xem phim trực tuyến!</span>
+          </h1>
+
+          <p className='px-3' style={{color: 'hsl(218, 81%, 85%)'}}>
+            Hãy đăng nhập để theo dõi lịch chiếu, đặt vé nhanh chóng, giữ chỗ tốt nhất và tận hưởng những ưu đãi dành riêng cho thành viên.
+          <br/> Trải nghiệm điện ảnh đỉnh cao chỉ với vài cú nhấp chuột!
           </p>
 
-          <MDBInput
-            wrapperClass="mb-4 w-100"
-            labelClass="text-[#1d1d1d]"
-            label="Số điện thoại"
-            id="formControlPhone"
-            type="text"
-            size="lg"
-          />
-          <MDBInput
-            wrapperClass="mb-4 w-100"
-            labelClass="text-[#1d1d1d]"
-            label="Mật khẩu"
-            id="formControlPassword"
-            type="password"
-            size="lg"
-          />
+        </MDBCol>
 
-          <p className="small mb-3">
-            <Link href="#!" className="text-[#1d1d1d] text-decoration-none">
-              Quên mật khẩu?
-            </Link>
-          </p>
+        <MDBCol md='4' className='position-relative'>
 
-          <MDBBtn
-            outline
-            className="w-100 rounded-pill"
-            size="lg"
-            style={{
-              border: '1px solid #007bff',
-              color: '#007bff',
-              fontFamily: 'Lato',
-              transition: '0.3s',
-            }}
-            onMouseOver={(e) => (e.target.style.backgroundColor = '#007bff', e.target.style.color = '#fff')}
-            onMouseOut={(e) => (e.target.style.backgroundColor = 'transparent', e.target.style.color = '#007bff')}
-          >
-            Đăng Nhập
-          </MDBBtn>
+          <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+          <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
 
-          <div className="d-flex flex-row mt-4 mb-4 justify-content-center">
-            <MDBBtn tag="a" color="none" className="m-2" style={{ color: '#1877f2' }}>
-              <FaFacebook style={{ width: '28px', height: '28px' }} />
-            </MDBBtn>
-            <MDBBtn tag="a" color="none" className="m-2" style={{ color: '#c32aa3' }}>
-              <FaInstagramSquare style={{ width: '28px', height: '28px' }} />
-            </MDBBtn>
-            <MDBBtn tag="a" color="none" className="m-2" style={{ color: '#ea4335' }}>
-              <FaGoogle style={{ width: '28px', height: '28px' }} />
-            </MDBBtn>
-          </div>
+          <MDBCard className='my-5 bg-glass w-full rounded-full'>
+            <MDBCardBody className='p-5'>
+                <div className="flex justify-between items-center w-full">
+              <h1 className="font-bold text-xl"style={{ fontFamily: "Lato", fontWeight: "bolder",fontSize: "20px" }}> Đăng Nhập </h1>
+              <Link href="/register" className="text-blue-600 hover:underline font-semibold transition duration-200"
+              style={{fontSize: "20px", fontFamily: "Lato", fontWeight: "bolder", textDecoration: "none", color: "#000"}}>Đăng ký</Link>
+              </div>
+              <MDBRow>
+                <MDBCol col='6'>
+                  <MDBInput wrapperClass='mb-4' label='Tài khoản' name='user' id='form1' type='text'/>
+                </MDBCol>
 
-          <p className="mb-0 text-muted">
-            Bạn chưa có tài khoản?{' '}
-            <Link href="register" className="fw-bold text-primary text-decoration-none">
-              Đăng ký
-            </Link>
-          </p>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
+              </MDBRow>
 
+              <MDBInput wrapperClass='mb-4' label='Email hoặc số điện thoại' name='email' id='form3' type='email'/>
+              <MDBInput wrapperClass='mb-4' label='Mật khẩu' name='password' id='form4' type='password'/>
+
+              <div className='d-flex justify-content-center mb-4'>
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Đăng ký nhận bản tin của chúng tôi' />
+              </div>
+             <button className="w-full rounded-full mb-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 transition duration-300"
+              style={{border: "none", borderRadius: "5px", fontFamily: "Lato", fontSize: "20px"}}>
+            Đăng nhập
+            </button>
+              <div className="text-center">
+
+                <p>Hoặc đăng nhập với:</p>
+
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1877f2' }}>
+                  <FaFacebook style={{width: "30px", height: "30px"}}/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#c32aa3' }}>
+                  <FaInstagram style={{width: "30px", height: "30px"}}/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#ea4335' }}>
+                 <FaGoogle style={{width: "30px", height: "30px"}}/>
+                </MDBBtn>
+              </div>
+
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+
+      </MDBRow>
+
+    </MDBContainer>
   );
 }
 
