cd32de18f04cc8470d3ec5c9fb3d41a776942427
enbesu <hau99082005@gmail.com>
2025-06-04 09:11:57 +0700

    Build admin dashboard

---
+++ app/admin-dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { Breadcrumb } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function AdminDashboard() {
+    return(
+     <div>
+        <Breadcrumb items={[{
+            label: "Dashboard",
+        }]}/>
+        <h1 className="text-4xl font-bold mt-6">Admin DashBoard</h1>
+        <Button asChild className="inline-flex pl-2 gap-2 mt-4">
+            <Link href={'/admin-dashboard/new'}><PlusCircleIcon/> New Property</Link>
+        </Button>
+     </div>
+    );
+}
