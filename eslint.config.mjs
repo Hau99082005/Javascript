e0d110e6a3ccdb06d496adb472f3c51a2271bfc4
enbesu <hau99082005@gmail.com>
2025-06-11 16:30:58 +0700

    master

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,32 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import compat from "@eslint/compat"; // nếu bạn định dùng `compat.extends`
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+    // Cấu hình cho JS
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        plugins: {
+            js
+        },
+        languageOptions: {
+            globals: globals.browser
+        },
+        extends: ["js/recommended"]
+    },
+
+    // Cấu hình cho React
+    pluginReact.configs.flat.recommended,
+
+    // Cấu hình mở rộng từ Next.js + TypeScript + custom rules
+    {
+        ...compat.extends("next/core-web-vitals", "next/typescript"),
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-empty-object-type": "off"
+        }
+    }
 ]);
